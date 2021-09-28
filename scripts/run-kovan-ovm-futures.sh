a2669fbf1de7778db2ca5f2ab3f590daab817598
liamzebedee <liamzebedee@yahoo.com.au>
2021-09-28 16:08:18 +1000

    auto-restart node

--- scripts/run-kovan-ovm-futures.sh
+++ scripts/run-kovan-ovm-futures.sh
@@ -1,3 +1,7 @@
 set -ex
 export NETWORK=kovan-ovm-futures 
-node src/ run -p https://kovan.optimism.io --from-block 0 -n 1
+
+until node src/ run -p https://kovan.optimism.io --from-block 0 -n 1; do
+    echo "Server 'myserver' crashed with exit code $?.  Respawning.." >&2
+    sleep 1
+done
