a55b2f114fb11175eadbac90974f21b9fff68084
liamzebedee <liamzebedee@yahoo.com.au>
2021-10-26 16:54:33 +1000

    * liquidate order in batches to reduce anti-DDoS from OVM

--- scripts/run-kovan-ovm-futures.sh
+++ scripts/run-kovan-ovm-futures.sh
@@ -3,5 +3,5 @@ export NETWORK=kovan-ovm-futures
 
 until node src/ run -p wss://ws-kovan.optimism.io --from-block 0 -n 1; do
     echo "Keeper exited with exit code $?.  Respawning.." >&2
-    sleep 3
+    sleep 15
 done
