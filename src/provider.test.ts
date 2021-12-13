ab2a48f2ea72cbc1cdd812bafd05e5837a11fcfc
Joachim Bachstätter <joachim.bachstatter@gmail.com>
2021-12-13 17:17:51 +1100

    Add test for monitorProvider, only websocket for now

--- src/provider.test.ts
+++ src/provider.test.ts
@@ -1,4 +1,4 @@
-import { getProvider } from "./provider";
+import { getProvider, monitorProvider } from "./provider";
 
 describe("provider", () => {
   describe("getProvider", () => {
@@ -63,6 +63,96 @@ describe("provider", () => {
       expect(() => {
         getProvider("bad url", deps);
       }).toThrowError("Invalid URL");
+    });
+  });
+  describe("monitorProvider", () => {
+    test("ws works", async () => {
+      jest.useFakeTimers();
+      const providerMock = {
+        _websocket: {
+          on: jest.fn().mockImplementation((event, cb) => {
+            // Trigger open event directly
+            if (event === "open") cb();
+            if (event === "pong") cb();
+          }),
+          terminate: jest.fn(),
+          ping: jest.fn(),
+        },
+      } as any;
+      const deps = {
+        WS_PROVIDER_TIMEOUT: 2 * 60 * 1000,
+        HEARTBEAT_INTERVAL: 3000,
+        ethNodeUptime: { set: jest.fn() },
+        ethNodeHeartbeatRTT: { observe: jest.fn() },
+      } as any;
+
+      const stopMonitoring = monitorProvider(providerMock, deps);
+
+      // Assert event listeners gets setup correctly
+      expect(providerMock._websocket.on).toBeCalledTimes(3);
+      expect(providerMock._websocket.on).toHaveBeenNthCalledWith(
+        1,
+        "open",
+        expect.any(Function)
+      );
+      expect(providerMock._websocket.on).toHaveBeenNthCalledWith(
+        2,
+        "pong",
+        expect.any(Function)
+      );
+      expect(providerMock._websocket.on).toHaveBeenNthCalledWith(
+        3,
+        "close",
+        expect.any(Function)
+      );
+      expect(providerMock._websocket.ping).not.toHaveBeenCalled();
+
+      // Assert ping gets called when heartbeat interval is triggered
+      jest.advanceTimersByTime(deps.HEARTBEAT_INTERVAL);
+      expect(providerMock._websocket.ping).toBeCalledTimes(1);
+
+      // Ensure the promises in the interval gets resolved
+      jest.useRealTimers();
+      await new Promise(process.nextTick);
+
+      // Assert that metrics gets called.
+      expect(deps.ethNodeUptime.set).toBeCalledTimes(1);
+      expect(deps.ethNodeUptime.set).toBeCalledWith(1);
+      expect(deps.ethNodeHeartbeatRTT.observe).toBeCalledTimes(1);
+      expect(deps.ethNodeHeartbeatRTT.observe).toBeCalledWith(
+        expect.any(Number)
+      );
+      // Stop monitoring to avoid jest worker process warning
+      stopMonitoring();
+    });
+    test("ws terminates and exits if open socket doesn't respond within WS_PROVIDER_TIMEOUT", () => {
+      jest.useFakeTimers();
+      const providerMock = {
+        _websocket: {
+          on: jest.fn().mockImplementation((event, cb) => {
+            // Trigger open event directly
+            if (event === "open") cb();
+            if (event === "pong") {
+            } // do not respond to pong event
+          }),
+          terminate: jest.fn(),
+          ping: jest.fn(),
+        },
+      } as any;
+      const deps = {
+        WS_PROVIDER_TIMEOUT: 2 * 60 * 1000,
+        HEARTBEAT_INTERVAL: 3000,
+        ethNodeUptime: { set: jest.fn() },
+        ethNodeHeartbeatRTT: { observe: jest.fn() },
+      } as any;
+      const processExitSpy = jest.spyOn(process, "exit").mockImplementation();
+      const stopMonitoring = monitorProvider(providerMock, deps);
+      jest.advanceTimersByTime(deps.WS_PROVIDER_TIMEOUT);
+
+      expect(processExitSpy).toBeCalledWith(1);
+      expect(providerMock._websocket.terminate).toBeCalled();
+      stopMonitoring();
+      jest.useRealTimers();
     });
   });
 });
