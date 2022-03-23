5cf02faae17233fd11d440554530fc066b8eb57c
joey <joey@cc.snxdao.io>
2022-03-23 13:27:42 +1100

    Add staging env file for keeper and use config file for pm2

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 "use strict";
 
+require("dotenv").config({
+  path:
+    // I would prefer to set NODE_ENV in ecosystem.config.js but the dot-env package and pm2 env configuration doesn't play nicely together
+    process.env.name === "futures-keeper-kovan"
+      ? require("path").resolve(__dirname, "../.env.staging")
+      : require("path").resolve(__dirname, "../.env"),
+});
 import { program } from "commander";
 import { cmd } from "./run";
 
 require("pretty-error").start();
-require("dotenv").config();
 
 cmd(program);
 
--- src/run/run.ts
+++ src/run/run.ts
@@ -1,4 +1,3 @@
-require("dotenv").config();
 import { NonceManager } from "@ethersproject/experimental";
 import snx from "synthetix";
 import Keeper from "../keeper";
