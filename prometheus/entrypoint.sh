3699874fe95d5e49d7dd8ec6bc33187069661062
David Vuong <david.vuong256@gmail.com>
2022-12-08 13:20:55 +1100

    rm: Drop prometheus

--- prometheus/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -ex
-
-# Generate config.
-/bin/confd -onetime -backend env
-
-# Run Prometheus.
-/bin/prometheus --web.config.file=/etc/prometheus/confd-prometheus-web.yml --config.file=/etc/prometheus/confd-prometheus.yml --log.level=debug
