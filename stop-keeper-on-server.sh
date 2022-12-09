012e3a6bb89c9b048d43ca5ba4a37931be461637
David Vuong <david.vuong256@gmail.com>
2022-12-09 11:50:47 +1100

    rm: prometheus ref

--- stop-keeper-on-server.sh
+++ stop-keeper-on-server.sh
@@ -5,5 +5,3 @@ LOCAL_FOLDER_NAME=`basename "$CURRENT"`
 
 echo "Stopping keeper"
 ssh -i "$PATH_TO_PRIVATE_KEY" "$USER_AT_IP" "cd $LOCAL_FOLDER_NAME;npx pm2 stop futures-keeper"
-echo "Stopping prometheus"
-ssh -i "$PATH_TO_PRIVATE_KEY" "$USER_AT_IP" "cd $LOCAL_FOLDER_NAME/prometheus;docker-compose stop"
