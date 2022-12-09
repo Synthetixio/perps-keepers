8d72dff000a10f40e3e85e9edb7f747571b0b8c8
David Vuong <david.vuong256@gmail.com>
2022-12-09 11:49:41 +1100

    rm: drop tools and cli (tbd on new cli)

--- tools/getLatestBlockNumber.js
+++
@@ -1,7 +0,0 @@
-const { providers } = require('ethers');
-
-const provider = new providers.JsonRpcProvider(process.argv[2]);
-provider
-  .getBlockNumber()
-  .then(console.log)
-  .catch(console.error);
--- tools/measure-ovm-speed.js
+++
@@ -1,5 +0,0 @@
-const ethers = require('ethers');
-const provider = new ethers.providers.WebSocketProvider(); // connect to localhost
-provider.on('block', blockNumber => {
-  console.log(blockNumber + ',' + +new Date());
-});
