fdb313a1309be72fc36952fb83ac5fc454eeb7e2
David Vuong <david.vuong256@gmail.com>
2022-12-20 16:23:44 +1100

    rm: rm -rf stop-keeper-on-server.sh

--- stop-keeper-on-server.sh
+++
@@ -1,7 +0,0 @@
-PATH_TO_PRIVATE_KEY=$1
-USER_AT_IP=$2
-CURRENT=`pwd`
-LOCAL_FOLDER_NAME=`basename "$CURRENT"`
-
-echo "Stopping keeper"
-ssh -i "$PATH_TO_PRIVATE_KEY" "$USER_AT_IP" "cd $LOCAL_FOLDER_NAME;npx pm2 stop perps-keeper"
