f6a70bd485917376ce612fab1e743d75705172b4
David Vuong <david.vuong256@gmail.com>
2023-01-09 13:06:31 +1100

    chore: A bit too low on gas. Specify something larger

--- src/keepers/delayedOffchainOrders.ts
+++ src/keepers/delayedOffchainOrders.ts
@@ -173,7 +173,7 @@ export class DelayedOffchainOrdersKeeper extends Keeper {
       });
       const tx = await this.market.executeOffchainDelayedOrder(account, priceUpdateData, {
         value: updateFee,
-        gasLimit: 15000,
+        gasLimit: 15_000_000,
       });
       this.logger.info('Successfully submitted execution transaction', {
         args: { account, nonce: tx.nonce },
