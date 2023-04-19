1009a505c2c3546fcbfc6e5c82d0c60eb1271d5e
David Vuong <david.vuong256@gmail.com>
2023-04-19 11:35:29 +1000

    feat+docs: Add config parsing and clarified docs

--- src/swap.ts
+++ src/swap.ts
@@ -2,11 +2,14 @@ import { providers } from 'ethers';
 import { createLogger } from './logging';
 import { Logger } from 'winston';
 import { Metrics } from './metrics';
+import { SignerPool } from './signerpool';
 
 export class TokenSwap {
   private readonly logger: Logger;
 
   constructor(
+    private readonly minSusdAmount: number,
+    private readonly signerPool: SignerPool,
     private readonly provider: providers.BaseProvider,
     private readonly metrics: Metrics
   ) {
@@ -21,7 +24,7 @@ export class TokenSwap {
     // Done.
   }
 
-  async listen(): Promise<void> {
+  async listen(interval: number): Promise<void> {
     this.logger.info('Start TokenSwap.listen for swaps');
     // Perform a setInterval which calls `swap` on some interval.
   }
